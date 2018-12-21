fd12ff8d20739d3a56268397b0dbba00912a0d88
shizhengfa <v191211@163.com>
2018-12-21 11:14:47 +0800

    add bridge design pattern

---
+++ src/main/java/com/journaldev/structural/bridge/Color.java
@@ -0,0 +1,6 @@
+package com.journaldev.structural.bridge;
+
+public interface Color {
+
+	public void applyColor();
+}
---
+++ src/main/java/com/journaldev/structural/bridge/RedColor.java
@@ -0,0 +1,8 @@
+package com.journaldev.structural.bridge;
+
+public class RedColor implements Color{
+
+	public void applyColor(){
+		System.out.println("red.");
+	}
+}
